0f9c2969c7a2daac40de0d193bce70df0a546ee4
NGergely7712 <nagy.gergely19991105@gmail.com>
2021-11-24 13:12:46 +0100

    feat: Add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import navbar from './Components/navbar.js';
 
 function App() {
   return (
     
     <div>
+      {navbar()}
       <h2>Hello world</h2>
     </div>
     
