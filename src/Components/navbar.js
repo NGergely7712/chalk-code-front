389c3342abbdb4d5fb9b0307cb86f5c3e5b02cf5
NGergely7712 <nagy.gergely19991105@gmail.com>
2021-11-25 11:18:51 +0100

    feat: Recreate navbar

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {MenuItems} from "./MenuItems"
+import './Navbar.css'
+class Navbar extends Component{
+    state ={clicked:false}
 
-function navbar() {
-    return (
-        <div>
-            <navbar>Cool School</navbar>
-        </div>
-    )
+    handleClick= () =>{
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    render(){
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">ChalkCode<i className="fab fa-react"></i></h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active': 'nav-menu'}>
+                    {MenuItems.map((item,index)=>{
+                        return(
+                            <li key={index}><a className={item.cname} href={item.url}>{item.title}</a></li>
+                        )
+                    })}
+                    
+                </ul>
+            </nav>
+        )
+    }
 }
-
-export default navbar
+export default Navbar
