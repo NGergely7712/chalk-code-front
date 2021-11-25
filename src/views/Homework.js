f2d275f1960175fe0b497c348cbc03e6f8cff98f
NGergely7712 <nagy.gergely19991105@gmail.com>
2021-11-25 14:41:26 +0100

    feat: Implement homework.js

--- src/views/Homework.js
+++ src/views/Homework.js
@@ -1,17 +1,40 @@
 import { render } from '@testing-library/react'
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import axios from 'axios'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
-function homework() {
-    return (
-        <>
-        <Navbar />
-        <div>
-            <h2>Hi from homework</h2>
-        </div>
-        </>
-    )
+function Homework() {
+    const url ='https://localhost:44324/school/teacher/4d4a57ee-dab3-4b40-b2ad-b9566d5adb45/showhomework'
+    const [Homework, sethomework] = useState(null);
+    useEffect(() => {
+        axios.get(url).then(response =>{
+            sethomework(response.data)
+        })
+        return () => {
+           
+        }
+    }, [url])
+
+    if(Homework){
+        return (
+            <>
+            <Navbar />
+            <div>
+                <h2>{Homework}</h2>
+            </div>
+            </>
+        )
+    }else{
+        return(
+            <div>
+                <Navbar />
+                <h2>Loading...</h2>
+            </div>
+        )
+    }
 }
 
 
-export default homework
+export default Homework
