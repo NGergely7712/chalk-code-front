b875c0e54bc2f5b1cd896c47e834fbaca3c48f2f
Orodbenn <Orodbenn@github.com>
2022-01-12 23:08:30 +0100

    chore: alter Sidebar.js methodology

--- src/zredo/Sidebar/Sidebar.js
+++ src/zredo/Sidebar/Sidebar.js
@@ -3,17 +3,39 @@ import SidebarBtns from './SidebarBtns.js'
 import "./Sidebar.css"
 import TableDisplay from "../ContentBody/TableDisplay";
 import ReactDOM from "react-dom";
+import ListClasses from "../ContentBody/ListClasses";
 
 class Sidebar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            table: null,
-            url: null
+            table: "",
+            url: ""
         }
     }
+
+    displayTable(table){
+        this.state.table = table
+        const container = document.getElementById("main-body-container")
+        return (
+            ListClasses()
+        )
+    }
     
-    
+    render() {
+        //const { userId, title, body } = this.state
+        return (
+            <div id="sidebar">
+                <ul>
+                    {SidebarBtns.map((item,index)=>{
+                        return(
+                            <li key={index}><button className={item.cname} onClick={this.displayTable(item.table)}>{item.title}</button></li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default Sidebar
